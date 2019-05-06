5e20f51753f76994af0ffc73d3049b2923531c48
crummy <crummynz@gmail.com>
2019-05-06 13:44:11 +1000

    CCF tests

---
+++ src/test/kotlin/com/malcolmcrum/gameboy/operations/CCFTest.kt
@@ -0,0 +1,26 @@
+package com.malcolmcrum.gameboy.operations
+
+import com.malcolmcrum.gameboy.Registers.Companion.CARRY_FLAG
+import com.malcolmcrum.gameboy.utils.State
+import org.junit.jupiter.api.Test
+
+@ExperimentalUnsignedTypes
+internal class CCFTest {
+    val opcode = 0x3f
+
+    @Test
+    fun `from set`() {
+        test(opcode) {
+            initial = State(f = CARRY_FLAG)
+            expected = State(f = 0u)
+        }
+    }
+
+    @Test
+    fun `from unset`() {
+        test(opcode) {
+            initial = State(f = 0u)
+            expected = State(f = CARRY_FLAG)
+        }
+    }
+}
